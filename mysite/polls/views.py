c0b87c7bc8b09b0f03157c982820bffde90bd613
shivank <skj9519@gmail.com>
2021-09-10 18:26:30 +0530

    PART 3: Views & templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+
+from .models import Question
 
 # Create your views here.
 
 
 def index(request):
-    print(request)
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # print(request)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    # template = loader.get_template('polls/index.html')
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    # print(request, question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {'question': question})
+
+
+def results(request, question_id):
+    response = "you're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting at question %s" % question_id)
